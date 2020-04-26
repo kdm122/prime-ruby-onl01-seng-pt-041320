6bcfccd371df5e7dbec8d206a1c5e4e442b2ca24
Kyle McLean <kyledmclean@gmail.com>
2020-04-26 01:20:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #number = 5
 # Add  code here!
 def prime?(number)
@@ -16,11 +17,27 @@ def prime?(number)
       else
 #        puts "its not 2 so false"
         return false
+=======
+number = 9
+# Add  code here!
+def prime(number)
+  checklist = [1..10]
+  n = number
+#  number.each do |n|
+    if n.even?
+      if n == 2 
+        puts "its 2 so true"
+        true 
+      else
+        puts "its not 2 so false"
+        false
+>>>>>>> d5ff5e27b52bd2d7ec4aa4e59ebc3baaf4dfd8d3
       end
     elsif  
       n.odd?
       checklist.each do |c|
         if n % c == 0 
+<<<<<<< HEAD
           if n == c || n == 1
       #      puts "disregarding number #{n} is equal to #{c}"
            check_array.push(true)
@@ -31,11 +48,19 @@ def prime?(number)
         else
       #    puts "#{n} could be prime tested against #{c}"
           check_array.push(true)
+=======
+          if n == c 
+            puts "disregarding number #{n} is equal to #{c}"
+          else
+            puts "not prime"
+          end
+>>>>>>> d5ff5e27b52bd2d7ec4aa4e59ebc3baaf4dfd8d3
         end
        end 
      end
       
 #    end
+<<<<<<< HEAD
     final_output = check_array.include?(false)
     if final_output == false
 #      puts "a prime"
@@ -48,3 +73,9 @@ def prime?(number)
 end
 
 #prime(number)
+=======
+      
+end
+
+prime(number)
+>>>>>>> d5ff5e27b52bd2d7ec4aa4e59ebc3baaf4dfd8d3
